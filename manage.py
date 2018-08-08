6bbac6b6ee945fb6197ed47c12d8562784316fd3
LingD <lingdong8023@sina.cn>
2018-08-08 18:15:22 +0800

    配置文件

--- manage.py
+++ manage.py
@@ -1,12 +1,34 @@
 from flask import Flask
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
+import redis
 
 app = Flask(__name__)
+manage = Manager(app)
+
+
+class Config(object):
+    DEBUG = True   # 设置debug
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@localhost"   # 设置绑定数据库
+    SQLALCHEMY_TRACK_MODIFICATIONS = False   # 追踪
+
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+    SESSION_TYPE = 'redis'   # 制定session保存到redis
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用redis的实例
+    SESSION_USE_SIGNER = True   # 签名加密
+    PERMANENT_SESSION_LEFETIME = 86400
+
+
+app.config.from_object(Config)    # 从类中调用配置文件
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
-    return '默认路由'
+    return 'index'
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    manage.run()
